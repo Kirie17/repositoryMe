8ebcfc5acfdaca459212b256ad19b70900fdd0db
Kirie17 <gui.ig.com@hotmail.com>
2024-09-16 15:39:07 -0300

    Update Principal.java

--- src/br/com/unit/Principal.java
+++ src/br/com/unit/Principal.java
@@ -1,1 +1,20 @@
-response.end("Hello World!");
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/seu_usuario/seu_repositorio.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn clean package' // Exemplo para projetos Java com Maven
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+    }
+}
